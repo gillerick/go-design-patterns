a93481785ab591056bc7753ad3fcbd4fa2e49126
Gill Erick <erick@sendyit.com>
2022-06-07 22:37:45 +0300

    GDP: Initial commit

---
+++ creational-patterns/builderImpl/builder.go
@@ -0,0 +1,17 @@
+package main
+
+type NotificationBuilder struct {
+	Title    string
+	Subtitle string
+	Message  string
+	Image    string
+	Icon     string
+	Priority int
+	NotType  string
+}
+
+func newNotificationBuilder() *NotificationBuilder {
+	return &NotificationBuilder{}
+}
+
+
