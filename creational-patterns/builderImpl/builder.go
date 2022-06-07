e2cf37a875bcf5db7c17b549dfa1d57c401c5c54
Gill Erick <erick@sendyit.com>
2022-06-07 22:44:39 +0300

    GDP: Initialize notification builder

--- creational-patterns/builderImpl/builder.go
+++ creational-patterns/builderImpl/builder.go
@@ -14,4 +14,34 @@ func newNotificationBuilder() *NotificationBuilder {
 	return &NotificationBuilder{}
 }
 
+func (nb *NotificationBuilder) setTitle(title string) {
+	nb.Title = title
+}
 
+func (nb *NotificationBuilder) setSubtitle(subtitle string) {
+	nb.Subtitle = subtitle
+}
+
+func (nb *NotificationBuilder) setMessage(message string) {
+	nb.Message = message
+}
+
+func (nb *NotificationBuilder) setImage(image string) {
+	nb.Image = image
+}
+
+func (nb *NotificationBuilder) setIcon(icon string) {
+	nb.Icon = icon
+}
+
+func (nb *NotificationBuilder) setPriority(priority int) {
+	nb.Priority = priority
+}
+
+func (nb *NotificationBuilder) setNotType(notType string) {
+	nb.NotType = notType
+}
+
+func (nb *NotificationBuilder) Build() (*Notification, error) {
+	return nil, nil
+}
