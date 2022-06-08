6537ecb65a200ba2e8a573f00ec0a02b2f6e5d75
Gill Erick <erick@sendyit.com>
2022-06-08 22:21:53 +0300

    GDP: Add intial logger implementation

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# go-design-patterns
+# Go Design Patterns
+
+****Singleton Pattern****
+
+The purpose of the singleton pattern is to restrict the instantiation of a class to a single instance and provide global
+access to that instance
+
+Other benefits
+
+1. Allows us to provide lazy instantiation (the class isn't instantiated until it is needed)
+2. Useful when you want to ensure that there's only one instance of a particular class in your application e.g in
+   logging, application configuration, telemetry & analytics, debugging
---
+++ creational-patterns/singletonImpl/singleton.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+type MyLogger struct {
+	loglevel int
+}
+
+//Log a message using the logger
+func (l *MyLogger) Log(s string) {
+	fmt.Println(l.loglevel, ":", s)
+}
+
+//SetLogLevel sets the log level of the logger
+func (l *MyLogger) SetLogLevel(level int) {
+	l.loglevel = level
+}
+
+//The logger instance
+var logger *MyLogger
+
+//ToDo: Use the sync package to enforce goroutine safety
+
+//ToDo: the getLoggerInstance function provides global access to the logger class instance
+func getLoggerInstance() *MyLogger {
+	return logger
+}
